8d8ea399b3d1e91625dbce0934bb7e837d3106ab
Besddev <besdinemail@gmail.com>
2025-05-16 23:02:45 +0300

    made smth to the main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
-}
+use rust_search::{SearchBuilder};
+
+// Made something to try this out
+ fn main() {
+     let search: Vec<String> = SearchBuilder::default()
+         .location("~/Downloads/")
+         .depth(1)
+         .build()
+         .collect();
+
+     for path in search {
+        println!("{:?}", path);
+     }
+ }
