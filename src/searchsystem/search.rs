bab382edcd5787cf6da2b9f28c27c8e2cdfaba8b
Besddev <besdinemail@gmail.com>
2025-05-19 20:25:30 +0300

    made the base file for the file searching

---
+++ src/searchsystem/search.rs
@@ -0,0 +1,15 @@
+use rust_search::{SearchBuilder};
+
+// Made something to try this out
+/*
+ fn main() {
+     let search: Vec<String> = SearchBuilder::default()
+         .location("~/Downloads/")
+         .depth(1)
+         .build()
+         .collect();
+
+     for path in search {
+        println!("{:?}", path);  
+     }
+ }*/
